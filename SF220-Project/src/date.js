ae895bef62282f6cf548f6d3637649a8b7574b4d
NarathipDev <narathip_aot@hotmail.com>
2022-04-27 09:00:11 +0700

    update

--- SF220-Project/src/date.js
+++ SF220-Project/src/date.js
@@ -2,7 +2,7 @@ import {writable} from 'svelte/store';
 
 let today = new Date();
 let final = new Date();
-final.setFullYear(2022, 4, 25);
+final.setFullYear(2022, 4, 30);
 final.setHours(23);
 final.setMinutes(0);
 final.setSeconds(0);
